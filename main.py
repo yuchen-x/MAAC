4b72aedc07012c93c0c72402fe8682607f0377ac
ycx424 <xiao.yuch@husky.neu.edu>
2020-12-07 15:41:14 -0500

    epi length 100

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
     parser.add_argument("--n_rollout_threads", default=4, type=int)
     parser.add_argument("--buffer_length", default=int(1e6), type=int)
     parser.add_argument("--n_episodes", default=50000, type=int)
-    parser.add_argument("--episode_length", default=25, type=int)
+    parser.add_argument("--episode_length", default=100, type=int)
     parser.add_argument("--steps_per_update", default=100, type=int)
     parser.add_argument("--num_updates", default=4, type=int,
                         help="Number of updates per update cycle")
